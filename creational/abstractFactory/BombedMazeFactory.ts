7e50129c28b34548b70767d673d7773b8fb7a5fc
iLoveHanekawa <arjunthakur900@gmail.com>
2024-02-26 01:12:43 +0530

    feat[abstractFactory]: RoomWithABomb

---
+++ creational/abstractFactory/BombedMazeFactory.ts
@@ -0,0 +1,32 @@
+// page 93
+
+import { Room } from "../Room.js";
+import { Wall } from "../Wall.js";
+import { MazeFactory } from "./MazeFactory.js";
+
+class RoomWithABomb extends Room {
+    constructor(roomNumber: number) {
+        super(roomNumber);
+    }
+}
+
+class BombedWall extends Wall {
+    constructor() {
+        super()
+    }
+}
+
+export class BombedMazeFactory extends MazeFactory {
+    constructor() {
+        super()
+    }
+
+    override MakeWall(): Wall {
+        return new BombedWall();
+    }
+
+    override MakeRoom(roomNumber: number): Room {
+        return new RoomWithABomb(roomNumber);
+    }
+
+}
