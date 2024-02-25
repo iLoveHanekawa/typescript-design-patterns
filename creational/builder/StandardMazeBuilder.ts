6572e8fc166b536ff9aaaaf71e9d0c9bdef66974
iLoveHanekawa <arjunthakur900@gmail.com>
2024-02-26 02:23:56 +0530

    fix[builder]: updated the standardMazeBuilder class

--- creational/builder/StandardMazeBuilder.ts
+++ creational/builder/StandardMazeBuilder.ts
@@ -1,8 +1,10 @@
 // page 102
 
 import { Direction } from "../Direction.js";
+import { Door } from "../Door.js";
 import { Maze } from "../Maze.js";
-import type { Room } from "../Room.js";
+import { Room } from "../Room.js";
+import { Wall } from "../Wall.js";
 import { MazeBuilder } from "./MazeBuilder.js";
 
 export class StandardMazeBuilder extends MazeBuilder {
@@ -10,9 +12,29 @@ export class StandardMazeBuilder extends MazeBuilder {
         super()
         this._currentMaze = new Maze();
     }
-    override BuildMaze(): void {}
-    override BuildRoom(room: number): void {}
-    override BuildDoor(roomFrom: number, roomTo: number): void {}
+    override BuildMaze(): void {
+        this._currentMaze = new Maze();
+    }
+    override BuildRoom(roomNumber: number): void {
+        if(!this._currentMaze.RoomNo(roomNumber)) {
+            const newRoom: Room = new Room(roomNumber);
+            this._currentMaze.AddRoom(newRoom);
+            const { East, West, North, South } = Direction;
+            newRoom.SetSide(North, new Wall());
+            newRoom.SetSide(South, new Wall());
+            newRoom.SetSide(East, new Wall());
+            newRoom.SetSide(West, new Wall());
+
+        }
+    }
+    override BuildDoor(roomFrom: number, roomTo: number): void {
+        const r1: Room = this._currentMaze.RoomNo(roomFrom);
+        const r2: Room = this._currentMaze.RoomNo(roomTo);
+        const aDoor: Door = new Door(r1, r2);
+        r1.SetSide(this.CommonWall(r1, r2), aDoor);
+        r1.SetSide(this.CommonWall(r2, r1), aDoor);
+    }
+    
     override GetMaze(): Maze {
         return this._currentMaze;
     }
