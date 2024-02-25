1a80c856752713b277fb19644b94dc1772d14365
iLoveHanekawa <arjunthakur900@gmail.com>
2024-02-25 21:30:19 +0530

    fix[creational]: fixed the room class to contain sides

--- creational/Room.ts
+++ creational/Room.ts
@@ -4,17 +4,21 @@ import { MapSite } from "./MapSite.js";
 import { Direction } from "./Direction.js";
 
 export class Room implements MapSite {
-    public _roomNumber: number;
-    
+    private _roomNumber: number;
+
     constructor(roomNo: number) {
-        this._roomNumber = roomNo
+        this._roomNumber = roomNo;
+        this._sides = [];
     }
     
-    public GetSide(direction: Direction): MapSite {
+    GetSide(direction: Direction): MapSite {
         // Subject to change
         return new Room(direction);
     }
 
-    public Enter(): void {}
+    Enter(): void {}
+
+    private _sides: number[];
+
     
 }
