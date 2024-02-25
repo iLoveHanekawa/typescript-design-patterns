21111dbf7d4c90cde721e35b8fc37d8202dbd5c1
iLoveHanekawa <arjunthakur900@gmail.com>
2024-02-26 02:38:57 +0530

    fix[builder]: fixed syntax errors on example2

--- creational/builder/Example2.ts
+++ creational/builder/Example2.ts
@@ -1,14 +1,11 @@
 import { MazeGame } from "./MazeGame.js";
 import { CountingMazeBuilder } from "./CountingMazeBuilder.js";
 
-const rooms: number = 0;
-const doors: number = 0;
-
 const game: MazeGame = new MazeGame();
 const builder: CountingMazeBuilder = new CountingMazeBuilder();
 
 game.CreateMaze(builder);
-const counts: { rooms: number; doors: number; } = { rooms: 0; doors: 0 };
+const counts: { rooms: number; doors: number; } = { rooms: 0, doors: 0 };
 builder.GetCounts(counts);
 
 console.log(counts);
