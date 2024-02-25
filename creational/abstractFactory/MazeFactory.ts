6e5c6cbda96eed7d7982c34e29b6eaee841d521e
iLoveHanekawa <arjunthakur900@gmail.com>
2024-02-26 00:51:00 +0530

    feat[creational]: MazeFactory

---
+++ creational/abstractFactory/MazeFactory.ts
@@ -0,0 +1,23 @@
+import { Door } from "../Door.js";
+import { Maze } from "../Maze.js";
+import { Room } from "../Room.js";
+import { Wall } from "../Wall.js";
+
+export class MazeFactory {
+
+    MakeMaze(): Maze {
+        return new Maze();
+    }
+
+    MakeWall(): Wall { 
+        return new Wall();
+    }
+
+    MakeRoom(roomNumber: number): Room {
+        return new Room(roomNumber);
+    }
+
+    MakeDoor(r1: Room, r2: Room): Door {
+        return new Door(r1, r2);
+    }
+}
