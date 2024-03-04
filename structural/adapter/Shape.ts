5161683b80fffbbd36d8b8ca4cba0894b47edc5e
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-04 09:25:44 +0530

    fix[adapter]: fixed Shape interface to support CreateManipulator method

--- structural/adapter/Shape.ts
+++ structural/adapter/Shape.ts
@@ -1,6 +1,7 @@
+import { Manipulator } from "./Manipulator.js";
 import { Point } from "./Point.js";
 
 export interface Shape {
     BoundingBox(bottomLeft: Point, topRight: Point): void;
-    
+    CreateManipulator(): Manipulator;
 }
