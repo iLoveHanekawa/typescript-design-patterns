066930f48fa3e1dfb0dd537a2088319febc92898
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-04 09:56:10 +0530

    fix[adapter]: fixed manipulator class to be abstract

--- structural/adapter/Manipulator.ts
+++ structural/adapter/Manipulator.ts
@@ -1,3 +1,3 @@
-export class Manipulator {
+export abstract class Manipulator {
     
 }
--- structural/adapter/TextShape.ts
+++ structural/adapter/TextShape.ts
@@ -2,8 +2,10 @@ import { TextView } from "./TextView.js";
 import { Shape } from "./Shape.js";
 import { Point } from "./Point.js";
 import { Coord } from "./Coord.js";
+import { Manipulator } from "./Manipulator.js";
 
 class TextShape extends TextView implements Shape {
+
     BoundingBox(bottomLeft: Point, topRight: Point): void {
         const bottom = new Coord();
         const left = new Coord();
@@ -13,5 +15,13 @@ class TextShape extends TextView implements Shape {
         this.GetExtent(width, height);
         bottomLeft = new Point(bottom, left);
         topRight = new Point(Coord.add(bottom, height), Coord.add(left, width));
+    }
+
+    IsEmpty(): boolean {
+        return super.IsEmpty();        
+    }
+
+    CreateManipulator(): import("./Manipulator.js").Manipulator {
+        
     }
 }
