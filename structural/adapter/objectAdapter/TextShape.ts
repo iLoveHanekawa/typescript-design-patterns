966721e154ce572d89a96003ae03dab869013117
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-05 14:27:31 +0530

    feat[adapter]: object adapter example, TextShape class

---
+++ structural/adapter/objectAdapter/TextShape.ts
@@ -0,0 +1,34 @@
+import { Coord } from "../classAdapter/Coord.js";
+import type { Manipulator } from "../classAdapter/Manipulator.js";
+import { Point } from "../classAdapter/Point.js";
+import { type Shape } from "../classAdapter/Shape.js";
+import { TextManipulator } from "../classAdapter/TextManipulator.js";
+import type { TextView } from "../classAdapter/TextView.js";
+
+export class TextShape implements Shape {
+    _text: TextView
+
+    constructor(textView: TextView) {
+        this._text = textView;
+    }
+
+    BoundingBox(bottomLeft: unknown, topRight: unknown): void {
+        const bottom = new Coord();
+        const left = new Coord();
+        const width = new Coord();
+        const height = new Coord();
+        this._text.GetOrigin(bottom, left);
+        this._text.GetExtent(width, height);
+        bottomLeft = new Point(bottom, left);
+        topRight = new Point(Coord.add(bottom, height), Coord.add(left, width));
+    }
+
+    IsEmpty(): boolean {
+        return this._text.IsEmpty();
+    }
+
+    CreateManipulator(): Manipulator {
+        return new TextManipulator(this);    
+    }
+
+}
