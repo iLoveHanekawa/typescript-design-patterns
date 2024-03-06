2dd84c8bef414f61da93c46357a7253d3178f7f3
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-06 10:49:04 +0530

    feat[builder]: abstract window class

--- structural/bridge/Window.ts
+++ structural/bridge/Window.ts
@@ -1,6 +1,36 @@
+// page 156
+
+import type { Point } from "../adapter/classAdapter/Point.js";
 import { View } from "./View.js";
+import { WindowImp } from "./WindowImp.js";
 
 export abstract class Window {
-    constructor(contents: View) {}
-    private GetView(): View { return new View(); }
+
+    constructor(contents: View) {
+        this._contents = contents;
+    }
+
+    private _contents: View;
+    private _imp: WindowImp | null = null;
+    
+    protected GetView(): View { return this._contents; }
+    protected GetWindowImp(): WindowImp { return this._imp; }
+    
+    // requests handled by window
+    abstract DrawContents(): void;
+    abstract Open(): void;
+    abstract Close(): void;
+    abstract Iconify(): void;
+    abstract Deiconify(): void;
+
+    // requests forwarded to implementation
+    abstract SetOrigin(at: Point): void;
+    abstract SetExtent(extent: Point): void;
+    abstract Raise(): void;
+    abstract Lower(): void;
+
+    abstract DrawLine(x: Point, y: Point): void;
+    abstract DrawRect(x: Point, y: Point): void;
+    abstract DrawPolygon(points: Point[], n: number): void;
+    abstract DrawText(char: string, x: Point): void;
 }
