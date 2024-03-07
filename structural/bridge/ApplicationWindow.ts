46f9720f4e68a9e4349628554d5da7f490bc26ec
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-07 15:16:26 +0530

    feat[bridge]: ApplicationWindowClass

---
+++ structural/bridge/ApplicationWindow.ts
@@ -0,0 +1,26 @@
+// page 157
+
+import { View } from "./View.js"
+import { Window } from "./Window.js"
+
+
+export class ApplicationWindow extends Window {
+    constructor() {
+        super(new View())
+    }
+
+    override Close(): void {}
+    override Open(): void {}
+    override Iconify(): void {}
+    override Deiconify(): void {}
+    override SetExtent(): void {}
+    override SetOrigin(): void {}
+    override Raise(): void {}
+    override Lower(): void {}
+    override DrawContents(): void {}
+    override DrawLine(): void {}
+    override DrawPolygon(): void {}
+    override DrawRect(): void {}
+    override DrawText(): void {}
+    
+}
