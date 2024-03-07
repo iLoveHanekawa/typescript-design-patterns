8a7daddbd5f595ebe320209ad051a99496b89979
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-07 15:09:55 +0530

    feat[bridge]: WindowImp implementation

--- structural/bridge/WindowImp.ts
+++ structural/bridge/WindowImp.ts
@@ -1,5 +1,17 @@
 // page 157
 
+import type { Coord } from "../adapter/classAdapter/Coord.js";
+
 export abstract class WindowImp {
-    constructor() {}
+
+    protected constructor() {}
+
+    abstract ImpTop(): void;
+    abstract ImpBottom(): void;
+    abstract ImpSetOrigin(): void;
+    abstract ImpSetExtent(): void;
+
+    abstract DeviceRect(x: Coord, y: Coord, w: Coord, h: Coord): void;
+    abstract DeviceText(char: string, x: Coord, y: Coord): void;
+    abstract DeviceBitmap(char: string, x: Coord, y: Coord): void;
 }
