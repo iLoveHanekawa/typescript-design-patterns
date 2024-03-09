ffa89f8a51fad96279a13cbbe12a182c79422d6a
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-09 11:28:40 +0530

    fix[composite]: fixed the currency class to have getters setters

--- structural/composite/Currency.ts
+++ structural/composite/Currency.ts
@@ -1,5 +1,12 @@
 // support class for various classes in the Composite chapter
 
 export class Currency {
+    private _value: number = 0;
     constructor() {}
+    get GetValue(): number {
+        return this._value;
+    }
+    set SetValue(n: number) {
+        this._value = n;
+    }
 }
