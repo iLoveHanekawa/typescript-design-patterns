867a078cce1516c982fdfa24cf83be13c2b46298
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-09 10:58:47 +0530

    feat[composite]: added floppy disk concrete class

--- structural/composite/FloppyDisk.ts
+++ structural/composite/FloppyDisk.ts
@@ -1,7 +1,15 @@
+// page 171
+
+import { Currency } from "./Currency.js";
 import { Equipment } from "./Equipment.js";
+import { Watt } from "./Watt.js";
 
 export class FloppyDisk extends Equipment {
     constructor(n: string) {
         super(n)
     }
+
+    override Power(): Watt { return new Watt(); }
+    override NetPrice(): Currency { return new Currency(); }
+    override DiscountPrice(): Currency { return new Currency(); }
 }
