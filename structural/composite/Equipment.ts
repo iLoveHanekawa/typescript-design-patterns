727047ea19d75a4d4e6ed21fecbf060a92e8b039
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-09 10:42:12 +0530

    feat[composite]: added support Iterator class

--- structural/composite/Equipment.ts
+++ structural/composite/Equipment.ts
@@ -1,5 +1,6 @@
 // page 170
 
+import type { Currency } from "./Currency.js";
 import type { Watt } from "./Watt.js";
 
 export abstract class Equipment {
@@ -7,6 +8,11 @@ export abstract class Equipment {
     Name(): string { return this._name; }
 
     abstract Power(): Watt;
+    abstract NetPrice(): Currency;
+    abstract DiscountPrice(): Currency;
+
+    abstract Add(e: Equipment): void;
+    abstract Remove(e: Equipment): void;
 
     protected constructor(n: string) {}
     private _name: string = '';
---
+++ structural/composite/Iterator.ts
@@ -0,0 +1,16 @@
+// support class for various classes in the Composite chapter
+
+import type { Equipment } from "./Equipment.js";
+
+export class Iterator<Type extends Equipment, Args extends any[]> {
+    // constructor for the argument Type
+
+    private _TCtor: { new(...args: Args): Type };
+    constructor(TCtor: { new(...args: Args): Type }) {
+        this._TCtor = TCtor
+    }
+
+    CreateIterator(...args: Args) {
+        return new this._TCtor(...args);
+    }
+}
