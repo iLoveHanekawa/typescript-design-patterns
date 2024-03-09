a6f8c4a197c91ee4518f57375234be4421f46dc1
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-09 12:17:07 +0530

    feat[composite]: added support class Cabinet

---
+++ structural/composite/Cabinet.ts
@@ -0,0 +1,9 @@
+// support class for page 172
+
+import { CompositeEquipment } from "./CompositeEquipment.js";
+
+export class Cabinet extends CompositeEquipment {
+    constructor(n?: string) {
+        super(n)
+    }
+}
